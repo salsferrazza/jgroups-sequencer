11c65a4dd88753baa94cfc343f864167f5e8194e
Salvatore Sferrazza <5541617+salsferrazza@users.noreply.github.com>
2024-11-27 00:31:42 -0500

    Add files via upload

---
+++ ItchMessage.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class ItchMessage implements Serializable {
+
+    private byte[] bytes;
+    private char messageType;
+    private long sequenceNumber; 
+    
+    public ItchMessage(byte[] bytes, long sequenceNumber) {
+	this.reset(bytes, sequenceNumber);
+    }
+    
+    public byte[] getBytes() {
+	return this.bytes;
+    }
+    
+    public int getSize() {
+	return this.bytes.length;
+    }
+
+    public char getType() {
+	return this.messageType;
+    }
+
+    public String toString() {
+	return this.messageType + " (" + this.getSize() + ")";
+    }
+
+    public void reset(byte[] bytes, long sequenceNumber) {	
+	this.sequenceNumber = sequenceNumber;
+	this.bytes = bytes;
+	// message type is the first byte of data
+	// - use bitwise operator to convert to char
+	if (bytes.length != 0) {
+	    this.messageType = (char) (0xff & bytes[0]);
+	}
+    }
+}
