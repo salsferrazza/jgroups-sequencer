663e03784f4e01678f6f52e6fc2c5ab96825a95f
Salvatore Sferrazza <5541617+salsferrazza@users.noreply.github.com>
2024-11-29 13:59:12 -0500

    Update Subscriber.java

--- Subscriber.java
+++ Subscriber.java
@@ -6,21 +6,20 @@ import java.net.URL;
 import java.io.*;
 import java.util.*;
 
-public class Subscriber extends StatefulClusterMember implements Receiver {
-
+public class Subscriber extends StatefulClusterMember {
+    
     public Subscriber(String configurationUrl, String clusterName) {
 	super(configurationUrl, clusterName);
     }
     
     @Override
     public void receive(Message msg) {
-	this.incrementState(msg);
+	this.incrementState(msg.getObject());
 	System.out.println(((ItchMessage) msg.getObject()).getType() + " " + msg.printHeaders());
     }
 
     @Override
     public void eventLoop() {
-	System.err.println("in sub event loop");
 	try {
 	    while (true) Thread.currentThread().yield();
 	} catch (Exception iex) {
@@ -33,21 +32,17 @@ public class Subscriber extends StatefulClusterMember implements Receiver {
 
 	String configUrl;
 	String clusterName;
-	
-	System.err.println("new sub " + args.length);
 
 	if (args.length == 2) {
 	    configUrl = args[0];
 	    clusterName = args[1];
-	    System.err.println("new sub 2");
 	    Subscriber sub = new Subscriber(configUrl, clusterName);
 	    System.err.println(sub);
 	    try {
-		System.err.println("new sub 3");
 		sub.start();
 	    } catch (Exception ex) {
-		System.err.println("new sub start exc");
 	    	ex.printStackTrace();
+		System.exit(1);
 	    }
 	} else {
 	    System.exit(1);
